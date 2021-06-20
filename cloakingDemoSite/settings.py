1bd0e5256cde9fa8ba59982370803988ddf371e4
Jeroen Pinoy <jeroen.pinoy@fortitude.ninja>
2021-06-20 16:36:40 -0700

    Added robots.txt

--- cloakingDemoSite/settings.py
+++ cloakingDemoSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!=(ic4zpz014c1hv8mn4-b#8+-b%by#(w)jq(wjlv#m-z4abb2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
