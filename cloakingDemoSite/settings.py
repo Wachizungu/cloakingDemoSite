7b673084ab07d2192173bbedfb8c20fbb66fd597
Jeroen Pinoy <jeroen.pinoy@fortitude.ninja>
2021-08-23 18:08:02 +0000

    Set static settings correctly to have admin gui working

--- cloakingDemoSite/settings.py
+++ cloakingDemoSite/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join('static'),)
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
